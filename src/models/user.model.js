53d62851566a87af1801a2509235f430ad8c5f31
AyushtheRock <ayushthegreate220@gmail.com>
2025-01-22 00:40:46 +0530

    Add new controller

---
+++ src/models/user.model.js
@@ -0,0 +1,59 @@
+import mongoose ,{Schema} from "mongoose";
+import { Trim } from './../../node_modules/mongoose/types/expressions.d';
+
+const userSchema = new Schema(
+    {
+      username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+      },
+      email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        },
+
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+          },
+          avatar:{
+            type:String,
+            required:true
+          },
+          coverimage:{
+            type:String,
+           
+          },
+          watchHistory:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,'Password is Required']
+        },
+
+        refreshToken: {
+            type:String
+        }
+
+    },
+    {
+        timestamps:true
+        
+    }
+)
+
+
+export const User = mongoose.model("User",userSchema)
